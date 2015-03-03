f2119b6b388c41538408bab2942ac77d33f8cd38
jkberger <jkberger@udel.edu>
2015-03-03 15:10:59 -0500

    Quiz 2 #16

---
+++ src/tuition/tuition.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+package tuition;
+
+public class tuition{
+
+	public static void main(String args[])
+	{
+		int t;
+		int initial;
+		int year;
+		int tuition;
+		System.out.println("Enter the tuition increase percentage in decimal form: ");
+		t = input.nextint();
+		
+		initial = 12342;
+		year = 1; 
+		for (int year <=4);
+				tuition = initial + (t*initial);
+				year += 1;
+				initial = tuition;
+		
+				System.out.println("the salary over four years is : " + tuition);
+	}
+}
+				
+				
+				
+				
+				
+				
+	
+
